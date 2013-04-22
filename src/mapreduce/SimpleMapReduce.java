91293e5efa3ba76796a8a64ddcb7ee2c34f459e9
Sweet Song <shihui.sweet.song@gmail.com>
2013-04-22 19:27:43 -0400

    Added Hadoop

--- src/mapreduce/SimpleMapReduce.java
+++ src/mapreduce/SimpleMapReduce.java
@@ -3,16 +3,22 @@ package mapreduce;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.StringTokenizer;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 
 public class SimpleMapReduce {
 
 	final static String FILE_NAME = "output.txt";
 	final static Charset ENCODING = StandardCharsets.UTF_8;
 	public static Node[] nodes;
-	
+
 	/**
 	 * @param args
 	 * @throws IOException 
@@ -21,10 +27,10 @@ public class SimpleMapReduce {
 	 */
 	public static void main(String[] args) throws IOException, AddressException, MessagingException {
 		try {
-		nodes = new Node[685230];
-		
-		Util.Init();
-		
+			nodes = new Node[685230];
+
+			Util.Init();
+
 		} catch (Exception e) {}
 		Util.email();
 	}
